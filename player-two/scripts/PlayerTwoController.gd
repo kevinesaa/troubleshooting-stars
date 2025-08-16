c639f955def996a765afaff307c315765d9d83f1
Kevin Esaa <kevin.esaa@gmail.com>
2025-08-15 23:12:23 -0400

    feature: player two self rotation

--- player-two/scripts/PlayerTwoController.gd
+++ player-two/scripts/PlayerTwoController.gd
@@ -1,53 +1,81 @@
 class_name PlayerTwoController
 extends Node2D
 
-@export_range(0,360,0.01)
-var initial_angle_position:float
-@export var base_angular_speed:float
-
 @onready var player_two_input_manager: PlayerTwoInputManager = $PlayerTwoInputManagerNode
 
-var input_position:float 
-var input_position_left:float
-var input_position_rigth:float
+#region orbit properties
+@export_range(0,360,0.01)
+var initial_angle_orbit_position:float
+@export 
+var base_angular_orbit_speed:float
 
+var input_orbit_position:float 
+var input_orbit_position_left:float
+var input_orbit_position_rigth:float
 
-var current_angle_position:float
+var current_angle_orbit_position:float
 var orbit_radius:float
-var angular_vector_position:Vector2 = Vector2.ZERO
+var angular_vector_orbit_position:Vector2 = Vector2.ZERO
 var pivot_player_node:Node2D
+#endregion
+
+#region self rotation properties
+@export
+var base_self_angular_speed:float
+var input_self_rotation:float 
+var input_self_rotation_left:float
+var input_self_rotation_rigth:float
+
+#endregion
 
 func on_press_left_orbit_listener(is_pressing:bool):
 	if(is_pressing):
-		self.input_position_left = -1
+		self.input_orbit_position_left = -1
 	else:
-		self.input_position_left = 0
+		self.input_orbit_position_left = 0
 
 func on_press_right_orbit_listener(is_pressing:bool):
 	if(is_pressing):
-		self.input_position_rigth = 1
+		self.input_orbit_position_rigth = 1
 	else:
-		self.input_position_rigth = 0
+		self.input_orbit_position_rigth = 0
+
+func on_press_left_self_rotation_listener(is_pressing:bool):
+	if(is_pressing):
+		self.input_self_rotation_left = -1
+	else:
+		self.input_self_rotation_left = 0
+
+func on_press_right_self_rotation_listener(is_pressing:bool):
+	if(is_pressing):
+		self.input_self_rotation_rigth = 1
+	else:
+		self.input_self_rotation_rigth = 0
 
 
 func _ready() -> void:
-	self.current_angle_position = self.initial_angle_position
+	self.current_angle_orbit_position = self.initial_angle_orbit_position
 	self.player_two_input_manager.pressing_orbit_right_notify.connect(on_press_right_orbit_listener)
 	self.player_two_input_manager.pressing_orbit_left_notify.connect(on_press_left_orbit_listener)
-		
+	self.player_two_input_manager.pressing_self_rotation_right_notify.connect(on_press_right_self_rotation_listener)
+	self.player_two_input_manager.pressing_self_rotation_left_notify.connect(on_press_left_self_rotation_listener )
+	
 
 func _process(delta: float) -> void:
-	self.input_position = self.input_position_rigth + self.input_position_left
+	self_rotation(delta)
 	if (self.pivot_player_node != null):
-		positioning(delta)
+		orbit_positioning(delta)
 	
+func self_rotation(delta_time:float) -> void:
+	self.input_self_rotation = self.input_self_rotation_rigth + self.input_self_rotation_left
+	var angular_speed = self.input_self_rotation * base_self_angular_speed * delta_time
+	angular_speed = deg_to_rad(angular_speed)
+	rotate(angular_speed)
 	
-	
-func positioning(delta_time:float) -> void:
-	
-	
-	var angular_speed = self.input_position * self.base_angular_speed * delta_time
-	self.current_angle_position = self.current_angle_position + angular_speed
-	angular_vector_position.x = self.orbit_radius * cos(deg_to_rad(self.current_angle_position))
-	angular_vector_position.y = self.orbit_radius * sin(deg_to_rad(self.current_angle_position))
-	self.position = self.pivot_player_node.position + angular_vector_position
+func orbit_positioning(delta_time:float) -> void:
+	self.input_orbit_position = self.input_orbit_position_rigth + self.input_orbit_position_left
+	var angular_speed = self.input_orbit_position * self.base_angular_orbit_speed * delta_time
+	self.current_angle_orbit_position = self.current_angle_orbit_position + angular_speed
+	angular_vector_orbit_position.x = self.orbit_radius * cos(deg_to_rad(self.current_angle_orbit_position))
+	angular_vector_orbit_position.y = self.orbit_radius * sin(deg_to_rad(self.current_angle_orbit_position))
+	self.position = self.pivot_player_node.position + angular_vector_orbit_position
--- player-two/scripts/PlayerTwoInputManager.gd
+++ player-two/scripts/PlayerTwoInputManager.gd
@@ -9,21 +9,26 @@ signal pressing_orbit_left_notify(isPressing:bool)
 #endregion
 
 #region face direcctions inputs
-signal pressing_(isPressing:bool)
-
+signal pressing_self_rotation_left_notify(isPressing:bool)
+signal pressing_self_rotation_right_notify(isPressing:bool)
 #endregion
+
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
 	
-	var right = Input.is_action_pressed("player_two_right")
+	var right_orbit = Input.is_action_pressed("player_two_right_orbit")
 	# var down = Input.is_action_pressed("player_one_down")
-	var left = Input.is_action_pressed("player_two_left")
+	var left_orbit = Input.is_action_pressed("player_two_left_orbit")
 	# var previus_weapon = Input.is_action_just_pressed("player_one_previus_weapon")
 	# var next_weapon = Input.is_action_just_pressed("player_one_next_weapon")
 	# var shooting = Input.is_action_pressed("player_one_shoot")
+	var self_rotation_left = Input.is_action_pressed("player_two_left_self_rotation") 
+	var self_rotation_right = Input.is_action_pressed("player_two_right_self_rotation") 
 	
-	pressing_orbit_right_notify.emit(right)
-	pressing_orbit_left_notify.emit(left)
+	pressing_orbit_right_notify.emit(right_orbit)
+	pressing_orbit_left_notify.emit(left_orbit)
+	pressing_self_rotation_left_notify.emit(self_rotation_left)
+	pressing_self_rotation_right_notify.emit(self_rotation_right)
 	
