3e112a93cb9ccd73a535b58b26f904a7d567c448
alejandro <alejandro@leadboxhq.com>
2025-08-29 22:42:57 -0400

    add spawner template

--- spawners/spawner.gd
+++ spawners/spawner.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @export var spawners: Array[Vector2] = []
+static var enemy_pool:PoolingSystemBase
+
+const COTUFINO = preload("res://enemies/cotufino/Cotufino.tscn")
 
 func _ready():
 	spawners = [
@@ -17,7 +20,23 @@ func _ready():
 		Vector2(192, -32),
 		Vector2(320, -32) #12
 	]
+	init_enemy_pool()
+	create_enemy()
 	pass
 
 func get_spawner(position):
 	return spawners[position]
+
+func init_enemy_pool():
+	if(enemy_pool == null):
+		enemy_pool = PoolingSystemBase.new(COTUFINO) 
+	var pool_parent_node = enemy_pool.get_parent()
+	if(pool_parent_node == null):
+		enemy_pool.call_deferred("add_pool_to_scene",get_tree().root)
+
+func create_enemy():
+	var cotufino:Cotufino = enemy_pool.get_object_from_pool() as Cotufino
+	cotufino.position = self.global_position
+
+func _process(delta: float):
+	pass
