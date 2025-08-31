46dc582a8bfc57db51b1b63ffc16d759d900f9c5
alejandro <alejandro@leadboxhq.com>
2025-08-30 23:13:47 -0400

    fix bullet collisions

--- bullet-systems/ale_bullets/ale_bullet.gd
+++ bullet-systems/ale_bullets/ale_bullet.gd
@@ -1,29 +1,14 @@
-extends Area2D
+extends BulletController
 class_name AleBullet
 
 @onready var speed:float = 4
 @onready var angle_in_degrees: float
 
-var poolContainer:PoolableObjectContainer
-var bullet_emitter:BulletEmitter
-enum BulletEmitter {
-	PLAYER = 2,
-	ENEMY = 4,
-}
-
-func set_emitter(emitter:BulletEmitter) -> void:
-	self.bullet_emitter = emitter
-	for i in range(0, 32):
-		set_collision_layer_value( i + 1,false)
-	set_collision_layer_value(emitter,true)
-
-func get_emitter() -> BulletEmitter:
-	return self.bullet_emitter
-
 func set_pool_container(poolContainer:PoolableObjectContainer):
 	self.poolContainer = poolContainer
 
 func back_to_pool(): 
+	print("aaaaa")
 	if(self.poolContainer != null):
 		self.poolContainer.back_to_pool()
 
